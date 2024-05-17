5f41d07220e495c12325242d8fa4bf997a83701c
cpshivhare <cpshivhare786@gmail.com>
2024-05-17 19:13:15 +0530

    Add test cases

---
+++ spec/calculator_spec.rb
@@ -0,0 +1,68 @@
+require_relative '../calculator'
+
+RSpec.describe Calculator do
+  describe 'Add string' do
+    context 'when given an empty string' do
+      it 'returns 0' do
+        string = Calculator.new
+        expect(string.add('')).to eq(0)
+      end
+    end
+
+    context 'when given a single number' do
+      it 'returns the number' do
+        calculator = Calculator.new
+        expect(calculator.add('1')).to eq(1)
+      end
+    end
+
+    context 'when given two numbers separated by a comma' do
+      it 'returns the sum of the numbers' do
+        calculator = Calculator.new
+        expect(calculator.add('1,2')).to eq(3)
+      end
+    end
+
+    context 'when given multiple numbers separated by commas and new lines' do
+      it 'returns the sum of the numbers' do
+        expect(Calculator.new.add("1\n2,3")).to eq(6)
+      end
+    end
+
+    context 'when given a custom delimiter' do
+      it 'returns the sum of the numbers using the custom delimiter' do
+        expect(Calculator.new.add("//;\n1;2")).to eq(3)
+      end
+    end
+
+    context 'when given a negative number' do
+      it "throws an exception 'negatives not allowed' and includes the negative number" do
+        expect { Calculator.new.add('1,-2') }.to raise_error(ArgumentError, 'negatives not allowed: -2')
+      end
+    end
+
+    context 'when given numbers larger than 1000' do
+      it 'ignores numbers larger than 1000 and returns the sum of the rest' do
+        expect(Calculator.new.add('2,1001')).to eq(2)
+      end
+    end
+
+    context 'when given a delimiter of any length' do
+      it 'returns the sum of the numbers using the delimiter' do
+        expect(Calculator.new.add("//[***]\n1***2***3")).to eq(6)
+      end
+    end
+
+    context 'when given multiple delimiters' do
+      it 'returns the sum of the numbers using the delimiters' do
+        expect(Calculator.new.add("//[*][%]\n1*2%3")).to eq(6)
+      end
+    end
+
+    context 'when given multiple delimiters with length longer than one character' do
+      it 'returns the sum of the numbers using the delimiters' do
+        expect(Calculator.new.add("//[**][%%]\n1**2%%3")).to eq(6)
+      end
+    end
+  end
+end
